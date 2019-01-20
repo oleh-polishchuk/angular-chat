1dc93351c9e4d3f891997e64f1f1b3c31f2c3dec
Oleh Polishchuk <oleh.polishchuk.ck@gmail.com>
2019-01-20 12:41:20 +0200

    init firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ChatComponent } from './components/chat/chat.component';
+import { ChatServiceService } from './components/shared/services/chat-service.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+const config = {
+  apiKey: 'AIzaSyAMUFM2zz24GHNJ_50kIvNHZpR6YWbKLy8',
+  authDomain: 'spdu-html-homework-10.firebaseapp.com',
+  databaseURL: 'https://spdu-html-homework-10.firebaseio.com',
+  projectId: 'spdu-html-homework-10',
+  storageBucket: 'spdu-html-homework-10.appspot.com',
+  messagingSenderId: '934812704997'
+};
 
 @NgModule({
   declarations: [
@@ -14,9 +29,17 @@ import { ChatComponent } from './components/chat/chat.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    AngularFireStorageModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    ChatServiceService,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
